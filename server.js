6fa0b390fe395fb81b23fa034c3577c3eff1361b
Nick Leon <nicolas7@vt.edu>
2021-08-14 21:27:23 -0400

    server js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const petData = require('./db/db.json');
+
+const PORT = "https://note-taker-nl92.herokuapp.com";
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at https://note-taker-nl92.herokuapp.com/`);
+});
